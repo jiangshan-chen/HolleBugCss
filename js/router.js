591528f3b569759743319c91d7b02d3d1e76102e
chenhongbin <chb430482@163.com>
2020-08-01 19:04:44 +0800

    初初始化提交

---
+++ js/router.js
@@ -0,0 +1,45 @@
+const router = [{
+		name: '首页',
+		path: 'main'
+	},
+	{
+		name: '第一页',
+		path: 'first'
+	},
+	{
+		name: '三角形',
+		path: 'pages/trigon'
+	},
+	{
+		name: '五边形',
+		path: 'pages/pentagon'
+	},
+	{
+		name: '气泡框',
+		path: 'pages/bubble_box'
+	},
+	{
+		name: '阴影',
+		path: 'pages/shadow'
+	},
+	{
+		name: '灰色滤镜',
+		path: 'pages/filter'
+	},
+	{
+		name:'渐变-斑马线',
+		path:'pages/shade'
+	},
+	{
+		name:'卡劵贴',
+		path:'pages/kajuan'
+	},
+	{
+		name:'文本省略',
+		path:'pages/text-omit'
+	},
+	{
+		name:'水中倒影',
+		path:'pages/reflexion'
+	}
+]
